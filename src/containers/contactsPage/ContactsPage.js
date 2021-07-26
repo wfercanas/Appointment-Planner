6f64187b5740f8f5b86a7b94d1ec80193aa3e012
wfercanas <fernando.canas@outlook.com>
2021-07-26 18:19:18 -0500

    add/ create contact form and its hooks

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,28 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = ({ contacts, createContact }) => {
+  const [currentName, setCurrentName] = useState('');
+  const [currentPhone, setCurrentPhone] = useState('');
+  const [currentEmail, setCurrentEmail] = useState('');
+
+  const [duplicatedName, setDuplicatedName] = useState(false);
+  useEffect(() => {
+    setDuplicatedName(contacts.some((contact) => contact.name === currentName));
+  }, [currentName, contacts]);
+
+  const handleChange = (e) => {
+    const setters = {
+      currentName: setCurrentName,
+      currentPhone: setCurrentPhone,
+      currentEmail: setCurrentEmail,
+    };
+
+    const stateToChange = e.target.name;
+    setters[stateToChange](e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
-  };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+    if (duplicatedName) {
+      return;
+    } else {
+      createContact(currentName, currentPhone, currentEmail);
+    }
+
+    setCurrentName('');
+    setCurrentPhone('');
+    setCurrentEmail('');
+  };
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <p style={{ color: 'red', fontSize: 10 }}>
+            {duplicatedName ? 'This name already exists as contact' : ''}
+          </p>
+          <input
+            type="text"
+            name="currentName"
+            placeholder="Insert new contact name"
+            value={currentName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="tel"
+            name="currentPhone"
+            placeholder="Insert new contact cellphone number (10 digits)"
+            maxLength="10"
+            pattern="[0-9]{10}"
+            value={currentPhone}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="currentEmail"
+            placeholder="Insert new contact email"
+            value={currentEmail}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit">Create contact</button>
+        </form>
       </section>
       <hr />
       <section>
