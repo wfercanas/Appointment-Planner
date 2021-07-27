9321de4967814d5d3825a897e1f3c77eefc2565d
wfercanas <fernando.canas@outlook.com>
2021-07-26 19:54:29 -0500

    add/ presentational form for creating apptmnt

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import { ContactPicker } from '../contactPicker/ContactPicker';
 
 export const AppointmentForm = ({
   contacts,
   title,
-  setTitle,
   contact,
-  setContact,
   date,
-  setDate,
   time,
-  setTime,
   handleSubmit,
+  handleChange,
 }) => {
   const getTodayString = () => {
     const [month, day, year] = new Date()
@@ -19,5 +17,32 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
   };
 
-  return AppointmentForm;
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        value={title}
+        onChange={handleChange}
+        required
+      />
+      <ContactPicker contacts={contacts} handleChange={handleChange} />
+      <input
+        name="date"
+        type="date"
+        value={date}
+        onChange={handleChange}
+        min={getTodayString()}
+        required
+      />
+      <input
+        name="time"
+        type="time"
+        value={time}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">Create appointment</button>
+    </form>
+  );
 };
