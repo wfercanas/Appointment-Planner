1e4d070a1925ed24a7f7cfb0efb4a45159e324cc
wfercanas <fernando.canas@outlook.com>
2021-07-26 19:55:03 -0500

    add/ control states when creating apptmnt

--- src/containers/appointmentsPage/appointmentsPage.js
+++ src/containers/appointmentsPage/appointmentsPage.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm';
+import { TileList } from '../../components/tileList/TileList';
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = ({
+  contacts,
+  appointments,
+  createAppointment,
+}) => {
+  const [title, setTitle] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
+  const handleChange = (e) => {
+    const setters = {
+      title: setTitle,
+      contact: setContact,
+      date: setDate,
+      time: setTime,
+    };
+
+    const stateToChange = e.target.name;
+    setters[stateToChange](e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
+    createAppointment(title, contact, date, time);
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          title={title}
+          contact={contact}
+          contacts={contacts}
+          date={date}
+          time={time}
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList appointments={appointments} />
       </section>
     </div>
   );
